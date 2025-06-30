03769e2ee6b14fd07e1f0265ba63b0fe09632a00
leonid <LaggerFint@yandex.ru>
2025-06-30 10:49:56 +0300

    тест нового вите конфиг

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
-    target: 'es2015', // важно!
+    target: 'es2015',  // обязательно
+    outDir: 'dist',
   },
 })
-
